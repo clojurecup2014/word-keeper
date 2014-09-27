5aba780ba765c1dc45a6421134bbdcee43465cbb
gadzhi <musaevgadzhi@gmail.com>
2014-09-27 16:48:58 +0400

    auth.clj

--- src/word_keeper/core.clj
+++ src/word_keeper/core.clj
@@ -3,20 +3,8 @@
             [compojure.core :refer [defroutes GET]]
             [compojure.handler :refer [site]]
             [clostache.parser :refer [render-resource]]
-            [oauth.client :as oauth]
             [word-keeper.backend  :refer :all]
             [word-keeper.frontend :refer :all]))
-
-(def consumer-key "5KvZggyamEy8yHD0oACgAkLxH")
-(def consumer-secret "3DeEHXQ6LVh7LxSdApivzAOiwBAcGdvRorheKzheCchbPPQF6h")
-(def consumer (oauth/make-consumer consumer-key
-                                   consumer-secret
-                                   "https://api.twitter.com/oauth/request_token"
-                                   "https://api.twitter.com/oauth/access_token"
-                                   "https://api.twitter.com/oauth/authorize"
-                                   :hmac-sha1))
-
-(def request-token (oauth/request-token consumer "http://word-keeper.clojurecup.com/signin"))
 
 (defroutes routes
   (GET "/" [] action-index))
