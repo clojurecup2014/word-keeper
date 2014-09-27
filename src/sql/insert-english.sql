72f338295a4e2ab7f3fe026165c7f719daa98aa3
endenis <endenis@gmail.com>
2014-09-28 00:37:33 +0200

    SQL for inserts

---
+++ src/sql/insert-english.sql
@@ -0,0 +1,11 @@
+WITH v AS (SELECT :word::text AS word)
+    ,s AS (SELECT id FROM English JOIN v USING (word))
+    ,i AS (
+       INSERT INTO English (word)
+       SELECT word
+       FROM   v
+       WHERE  NOT EXISTS (SELECT * FROM s)
+       RETURNING id)
+SELECT id FROM i
+UNION  ALL
+SELECT id FROM s;
