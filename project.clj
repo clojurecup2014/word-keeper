4672e27886f9c5409d9858fc637cc537dafc1372
gadzhi <musaevgadzhi@gmail.com>
2014-09-27 04:10:19 +0400

    Added dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [http-kit "2.1.16"]
+                 [compojure "1.1.9"]
+                 [org.clojure/clojurescript "0.0-2342"]
+                 [om "0.7.3"]
+                 [yesql "0.4.0"]
+                 [org.postgresql/postgresql "9.3-1102-jdbc41"]
+                 [ring "1.2.0"]
+                 [cheshire "5.3.1"]
+                 [de.ubercode.clostache/clostache "1.4.0"]])
