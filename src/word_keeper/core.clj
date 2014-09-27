21da4934badbf32065e86f36b3b0821100c5d608
gadzhi <musaevgadzhi@gmail.com>
2014-09-27 16:22:03 +0400

    clj-oauth added

--- src/word_keeper/core.clj
+++ src/word_keeper/core.clj
@@ -2,7 +2,13 @@
   (:require [org.httpkit.server :refer [run-server]]
             [compojure.core :refer [defroutes GET]]
             [compojure.handler :refer [site]]
-            [clostache.parser :refer [render-resource]]))
+            [clostache.parser :refer [render-resource]]
+            [oauth.client :as oauth]))
+
+(def consumer-key "5KvZggyamEy8yHD0oACgAkLxH")
+(def consumer-secret "3DeEHXQ6LVh7LxSdApivzAOiwBAcGdvRorheKzheCchbPPQF6h")
+
+
 
 (defn action-index [req]
   {:status 200
