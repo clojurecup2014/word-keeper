2ed5ad08f4f2015519df2823ce7a143d2cebeccc
endenis <endenis@gmail.com>
2014-09-27 14:57:53 +0200

    Initial bootstrap markup

--- src/word_keeper/core.clj
+++ src/word_keeper/core.clj
@@ -2,6 +2,7 @@
   (:require [org.httpkit.server :refer [run-server]]
             [compojure.core :refer [defroutes GET]]
             [compojure.handler :refer [site]]
+            [compojure.route :refer [files not-found]]
             [clostache.parser :refer [render-resource]]
             [oauth.client :as oauth]
             [word-keeper.backend  :refer :all]
@@ -10,15 +11,10 @@
 (def consumer-key "5KvZggyamEy8yHD0oACgAkLxH")
 (def consumer-secret "3DeEHXQ6LVh7LxSdApivzAOiwBAcGdvRorheKzheCchbPPQF6h")
 
-(defn action-index [req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (render-resource
-            "views/index.html.mustache"
-            nil)})
-
 (defroutes routes
-  (GET "/" [] action-index))
+  (GET "/" [] action-index)
+  (files "/public/")
+  (not-found "<h1>404. Not found</h1>"))
 
 (defn -main [& args]
   (run-server (site #'routes) {:port 8080}))
