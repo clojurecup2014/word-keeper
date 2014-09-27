0dc1e29a605038055e308e7c919fb8237eaeca3b
gadzhi <musaevgadzhi@gmail.com>
2014-09-28 01:43:58 +0400

    Bug with users/twitter-users fixed

--- src/word_keeper/db.clj
+++ src/word_keeper/db.clj
@@ -23,13 +23,13 @@
 (defquery select-twitter-user "sql/select-twitter-user.sql")
 (defn find-twitter-user [twitter-id] (first (select-twitter-user db-spec twitter-id)))
 
-(defquery insert-user<! "sql/insert-user.sql")
+(defquery insert-user "sql/insert-user.sql")
 (defn create-user! [] (insert-user<! db-spec))
 
 (defquery insert-twitter-user! "sql/insert-twitter-user.sql")
 (defn create-twitter-user!
   ([twitter-id twitter-name]
-     (let [uid (-> (create-user!) first :id)]
+     (let [uid (-> (create-user!) :id)]
        (create-twitter-user! twitter-id twitter-name uid)))
   ([twitter-id twitter-name uid]
      (insert-twitter-user! db-spec twitter-id twitter-name uid)))
